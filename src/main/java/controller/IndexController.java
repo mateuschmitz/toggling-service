bc4e730c27682af5c0e42b952081d6fc48c5f3b9
Mateus Silveira <mateus.silveira@zenvia.com>
2019-11-27 21:29:27 -0300

    Added initial controller to test api flow

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import io.javalin.Javalin;
+
+public class IndexController {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Testing structure"));
+    }
+}
